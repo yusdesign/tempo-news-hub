0a4276eace8dca45f524d35b27a4778cc071599f
yusdesign <swiftcase@gmail.com>
2025-11-17 16:54:08 +0800

    Add debug toast to see if app starts

--- app/src/main/java/com/tempo/newshub/MainActivity.java
+++ app/src/main/java/com/tempo/newshub/MainActivity.java
@@ -3,38 +3,23 @@ package com.tempo.newshub;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    
-    private WebView webView;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Create WebView programmatically - NO XML
-        webView = new WebView(this);
+        // DEBUG: Show we're alive
+        Toast.makeText(this, "🚀 APP STARTED!", Toast.LENGTH_LONG).show();
         
-        // Enable JavaScript
+        WebView webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
-        
-        // Keep navigation in the app
         webView.setWebViewClient(new WebViewClient());
-        
-        // Load our local HTML file
         webView.loadUrl("file:///android_asset/news_app.html");
         
         setContentView(webView);
-    }
-    
-    @Override
-    public void onBackPressed() {
-        // Handle back button in WebView
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
     }
 }
