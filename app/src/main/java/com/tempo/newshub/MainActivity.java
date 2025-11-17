b2f7aabe0ebe2e53818a3f0148b68afb6d3f190e
yusdesign <swiftcase@gmail.com>
2025-11-18 05:28:09 +0800

    v2.1.0: Add daytime greetings system + updated UI

--- app/src/main/java/com/tempo/newshub/MainActivity.java
+++ app/src/main/java/com/tempo/newshub/MainActivity.java
@@ -5,15 +5,32 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     
+    private String getDaytimeGreeting() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        
+        if (hour >= 5 && hour < 12) {
+            return "Good morning";
+        } else if (hour >= 12 && hour < 18) {
+            return "Good afternoon"; 
+        } else {
+            return "Good evening";
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         Toast.makeText(this, "🚀 GUARDIAN CHARGE!", Toast.LENGTH_LONG).show();
         
+        String greeting = getDaytimeGreeting();
+        Toast.makeText(this, "🌅 " + greeting + "!", Toast.LENGTH_SHORT).show();
+
         try {
             WebView webView = new WebView(this);
             webView.getSettings().setJavaScriptEnabled(true);
@@ -25,9 +42,15 @@ public class MainActivity extends AppCompatActivity {
                     view.loadUrl(url);
                     return true;
                 }
-                
+
                 @Override
                 public void onPageFinished(WebView view, String url) {
+                    // Inject greeting into the WebView
+                    String js = "javascript:if(document.getElementById('greeting')) { " +
+                                "document.getElementById('greeting').innerText = '" + greeting + "'; " +
+                                "}";
+                    view.evaluateJavascript(js, null);
+                    
                     if (url.startsWith("file://")) {
                         Toast.makeText(MainActivity.this, "📰 Guardian Edition Loaded!", Toast.LENGTH_SHORT).show();
                     } else {
