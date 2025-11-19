2956978e3ef3bbed5b640057be4c74e117e277fc
yusdesign <swiftcase@gmail.com>
2025-11-20 06:18:53 +0800

    BACKUP: Before complete reset

--- app/src/main/java/com/tempo/newshub/NewsArticle.java
+++
@@ -1,9 +0,0 @@
-package com.tempo.newshub;
-
-public class NewsArticle {
-    public String title;
-    public String url;
-    public String date;
-    public String source;
-    public String image;
-}
